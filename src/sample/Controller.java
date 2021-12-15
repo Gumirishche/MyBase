6f82250430d7a3b23da9238adfa8a664b2122816
Gumirishche <gumer2001@mail.ru>
2021-12-15 16:40:01 +0400

    сделал внешку и установил драйвер для sql

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,6 +3,9 @@ package sample;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 
 public class Controller {
 
@@ -13,7 +16,19 @@ public class Controller {
     private URL location;
 
     @FXML
+    private Button addButton;
+
+    @FXML
+    private TextField textField;
+
+    @FXML
+    private VBox VBox;
+
+    @FXML
     void initialize() {
+        assert addButton != null : "fx:id=\"addButton\" was not injected: check your FXML file 'sample.fxml'.";
+        assert textField != null : "fx:id=\"textField\" was not injected: check your FXML file 'sample.fxml'.";
+        assert VBox != null : "fx:id=\"VBox\" was not injected: check your FXML file 'sample.fxml'.";
 
     }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Список дел");
+        primaryStage.setScene(new Scene(root, 268, 427));
         primaryStage.show();
     }
 
