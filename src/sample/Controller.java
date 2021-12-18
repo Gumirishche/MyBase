f2489c0a47f29ed2d06fcdf5d0eefda56a27f44f
Gumirishche <gumer2001@mail.ru>
2021-12-18 23:34:55 +0400

    добавил очистку и очистил код

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -5,7 +5,6 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
 
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -31,52 +30,43 @@ public class Controller {
     @FXML
     private VBox allThings;
 
+    @FXML
+    private Button clearButton;
+
     DB db = null;
 
     @FXML
     void initialize() {
         db = new DB();
-
-        // Обработчик события. Сработает при нажатии на кнопку
-        addButton.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
-            // Метод, что будет срабатывать
-            @Override
-            public void handle(MouseEvent mouseEvent) {
-                try {
-                    // Проверяем является ли поле заполненным
-                    if(!textField.getText().trim().equals("")) {
-                        // Вызываем метод из класса DB
-                        // через этот метод будет добавлено новое задание
-                        db.insertTask(textField.getText());
-                        loadInfo(); // Метод для подгрузки заданий внутрь программы
-                        textField.setText(""); // Очищаем поле
-                    }
-                } catch (SQLException e) { // Отслеживаем ошибки
-                    e.printStackTrace();
-                } catch (ClassNotFoundException e) {
-                    e.printStackTrace();
+        addButton.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent -> {
+            try {
+                if (!textField.getText().trim().equals("")) {
+                    db.insertTask(textField.getText());
+                    loadInfo();
+                    textField.setText("");
+                    loadInfo();
                 }
+            } catch (SQLException e) {
+                e.printStackTrace();
             }
         });
-
-        // Метод для подгрузки заданий внутрь программы
+        clearButton.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent -> {
+            try {
+                db.clearTable();
+                loadInfo();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        });
         loadInfo();
     }
 
-    // Метод для подгрузки заданий внутрь программы
     void loadInfo() {
         try {
-            // Сначала очищаем от прошлых значений
             allThings.getChildren().clear();
-
-            // Получаем все задания из базы данных
             ArrayList<String> tasks = db.getTasks();
-            for(int i = 0; i < tasks.size(); i++) // Перебираем их через цикл
-                // Добавляем каждое задание в объект VBox (allThings)
-                allThings.getChildren().add( new Label( tasks.get(i)));
+            for (String task : tasks) allThings.getChildren().add(new Label(task));
         } catch (SQLException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
     }
--- src/sample/db/DB.java
+++ src/sample/db/DB.java
@@ -5,23 +5,31 @@ import java.util.ArrayList;
 
 public class DB {
 
-    public void insertTask(String task) throws SQLException, ClassNotFoundException{
-        String sql = "INSERT INTO mydealstable (deal) VALUES(?)";
+    public void insertTask(String task) throws SQLException {
+        String sql = "INSERT INTO tab (did) VALUES(?)";
 
-        PreparedStatement prSt = DriverManager.getConnection("jdbc:postgresql://localhost:5432/mydeals","postgres","123").prepareStatement(sql);
-        prSt.setString(1,task);
+        PreparedStatement prSt = DriverManager.getConnection("jdbc:postgresql://localhost:5432/mydeals", "postgres", "123").prepareStatement(sql);
+        prSt.setString(1, task);
 
         prSt.executeUpdate();
     }
 
-    public ArrayList<String> getTasks() throws SQLException, ClassNotFoundException{
-        String sql="SELECT * FROM mydealstable ORDER BY id DESC";
-        Statement statement= DriverManager.getConnection("jdbc:postgresql://localhost:5432/mydeals","postgres","123").createStatement();
-        ResultSet res =statement.executeQuery(sql);
+    public void clearTable() throws SQLException {
+        String sql = "DELETE FROM tab";
 
-        ArrayList<String> tasks=new ArrayList<>();
-        while (res.next()){
-            tasks.add(res.getString("deal"));
+        PreparedStatement prSt = DriverManager.getConnection("jdbc:postgresql://localhost:5432/mydeals", "postgres", "123").prepareStatement(sql);
+
+        prSt.executeUpdate();
+    }
+
+    public ArrayList<String> getTasks() throws SQLException {
+        String sql = "SELECT * FROM tab ORDER BY id DESC";
+        Statement statement = DriverManager.getConnection("jdbc:postgresql://localhost:5432/mydeals", "postgres", "123").createStatement();
+        ResultSet res = statement.executeQuery(sql);
+
+        ArrayList<String> tasks = new ArrayList<>();
+        while (res.next()) {
+            tasks.add(res.getString("did"));
         }
         return tasks;
     }
